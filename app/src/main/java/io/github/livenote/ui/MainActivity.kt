040a441ad8e389d1d53a5bd4fdf3753252e35269
vitalir2 <vitalyi100@mail.ru>
2021-07-25 14:43:04 +0300

    Implement default navigation

--- app/src/main/java/io/github/livenote/ui/MainActivity.kt
+++ app/src/main/java/io/github/livenote/ui/MainActivity.kt
@@ -1,14 +1,59 @@
-package io.github.livenote.ui
+    package io.github.livenote.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.livenote.R
+import io.github.livenote.databinding.ActivityMainBinding
 
-@AndroidEntryPoint
+
+    @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.mainFragment),
+            binding.drawerLayout
+        )
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        configureToolbar(navController)
+        configureNavigationDrawer(navController)
+    }
+
+    private fun configureToolbar(navController: NavController) {
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.top_app_bar, menu)
+        return true
+    }
+
+    private fun configureNavigationDrawer(navController: NavController) {
+        binding.mainNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
     }
 }

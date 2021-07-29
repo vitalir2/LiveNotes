bb976f723e569d95fb144e60f9702d266ceb3f25
Semper0n <tyutchevivan@rambler.ru>
2021-07-29 14:28:40 +0300

    Add a forgotten ItemAdapter

---
+++ app/src/main/java/io/github/livenote/ui/adapter/ItemAdapter.kt
@@ -0,0 +1,33 @@
+package io.github.livenote.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.github.livenote.data.models.Note
+import io.github.livenote.R
+
+class ItemAdapter(
+    private val dataset: List<Note>
+    ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textViewTitle: TextView = view.findViewById(R.id.title_text)
+        val textViewDate: TextView = view.findViewById(R.id.date_text)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_note, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textViewTitle.text = item.name
+    }
+
+    override fun getItemCount() = dataset.size
+}

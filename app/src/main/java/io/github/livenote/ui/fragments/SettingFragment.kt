4e9e64e6969c27cb3fb8d73a842c6ef501fb7bab
Semper0n <tyutchevivan@rambler.ru>
2021-08-16 15:55:32 +0300

    Add theme switch

--- app/src/main/java/io/github/livenote/ui/fragments/SettingFragment.kt
+++ app/src/main/java/io/github/livenote/ui/fragments/SettingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import io.github.livenote.R
 import io.github.livenote.databinding.FragmentMainBinding
 import io.github.livenote.databinding.FragmentSettingBinding
@@ -18,6 +19,14 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSettingBinding.inflate(inflater, container, false)
+
+        binding.darkThemeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
         return binding.root
     }
 }

4d610ca7b5873da2b36e5137c8e870066cc86c83
Semper0n <tyutchevivan@rambler.ru>
2021-08-18 16:56:11 +0300

    Fix SettingFragment.kt Made a memorization of the switch parameter

--- app/src/main/java/io/github/livenote/ui/fragments/SettingFragment.kt
+++ app/src/main/java/io/github/livenote/ui/fragments/SettingFragment.kt
@@ -1,5 +1,6 @@
 package io.github.livenote.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -20,13 +21,19 @@ class SettingFragment : Fragment() {
     ): View {
         binding = FragmentSettingBinding.inflate(inflater, container, false)
 
+        val sharedPreferences = activity?.getSharedPreferences("SWITCH_COND", Context.MODE_PRIVATE)
+
         binding.darkThemeSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
+            if (isChecked)
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            }
             else
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            val switchCond = binding.darkThemeSwitch.isChecked
+            val editor = sharedPreferences?.edit()
+            editor?.putBoolean("SWITCH_COND", switchCond)
+            editor?.apply()
         }
+        sharedPreferences?.getBoolean("SWITCH_COND", false)
         return binding.root
     }
 }

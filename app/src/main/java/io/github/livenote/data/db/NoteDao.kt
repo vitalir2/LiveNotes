1ab9e2d0f0c9f79eb4752248f28dfe61c6e9abc6
vitalir2 <vitalyi100@mail.ru>
2021-08-27 17:10:09 +0300

    Fix saving the same name note

--- app/src/main/java/io/github/livenote/data/db/NoteDao.kt
+++ app/src/main/java/io/github/livenote/data/db/NoteDao.kt
@@ -1,15 +1,12 @@
 package io.github.livenote.data.db
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import io.github.livenote.data.models.NoteDb
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(note: NoteDb)
 
     @Delete
